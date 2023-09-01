4f2263c1a05b3537eb74f8211684bc3006456e1e
Burakgokmen19 <burakgokmn19@gmail.com>
2023-09-01 16:47:03 +0300

    edit

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,68 @@
 <template>
-  <Tutorial/>
+  <!doctype html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <title>Document</title>
+  </head>
+  <body>
+  <div class="login-container">
+    <div class="login-box">
+      <h2>Login</h2>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" id="email" v-model="email" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model="password" required>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      <div>
+        <a href="/register">register</a>
+      </div>
+    </div>
+  </div>
+  </body>
+  </html>
+
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const response = await this.$axios.post('/api/login', {
+          email: this.email,
+          password: this.password
+        });
+
+        // İstediğiniz işlemi yapabilirsiniz (örneğin: yönlendirme)
+        console.log('Logged in:', response.data);
+
+      } catch (error) {
+        console.error('Login error:', error);
+      }
+    }
+  }
+};
 </script>
+
+<style scoped>
+/* Stillemeler buraya gelebilir */
+</style>
